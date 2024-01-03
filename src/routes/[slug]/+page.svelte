f0d2a89f4ff317e9d3282b635c8ca47b3d2a7494
aksharye <aksharye@gmail.com>
2024-01-03 16:11:30 -0500

    screenshare bug fixes and closing unclosed connections

--- src/routes/[slug]/+page.svelte
+++ src/routes/[slug]/+page.svelte
@@ -1,3 +1,7 @@
+<style>
+
+</style>
+
 <script lang='ts'>
 	import { onMount } from 'svelte';
 	import { io } from 'socket.io-client'
@@ -7,24 +11,38 @@
 
 	export let data;
     let roomName = data.name;
+
 	const peers = {};
 	const videos = {};
-	const shares = {};
-	const sharecalls = {};
+
+	const sharesIn = {};
+	const shareCallsOut = {};
+	const shareCallsIn = {};
+
+	var camera = false;
 	var muted = false;
+	var screenshare = false;
+	var screenshareTracks = null;
+	var myShare = null;
 
 	onMount(() => {
 
 		
-		const socket = io("https://fluorescent-profuse-acorn.glitch.me");
+		// const socket = io("https://fluorescent-profuse-acorn.glitch.me");
+		const socket = io("http://localhost:3000");
 
 		const videoGrid = document.getElementById('video-grid');
 		const superVid = document.getElementById('super-video');
+		
+		superVid.muted = true;
+	
 
 		socket.on("connect", () => {
 			console.log("Successfully connected to socket");
 		});
+
 		var myId = "";
+		
 		import("peerjs").then(({ default: Peer }) => {
 			const peer = new Peer();
 
@@ -40,12 +58,14 @@
 				video: true,
 				audio: true
 			}).then(stream => {
-
+				
 				// create video object, add to stream
 				const video = document.createElement("video");
 				video.muted = true;
 				superVid.srcObject = stream;
-				addStream(video, stream);
+				superVid.classList.add("-scale-x-100")
+				addStream(video, stream, true);
+
 
 				function toggleCam() {
 					stream.getVideoTracks()[0].enabled = !stream.getVideoTracks()[0].enabled;
@@ -66,22 +86,37 @@
 					const video = document.createElement("video");
 					let streamType = call.metadata.type;
 					if (streamType == "video") {
+						peers[call.peer] = call;
 						videos[call.peer] = video;
 						call.answer(stream)
 					} else {
-						shares[call.peer] = video;
+						shareCallsIn[call.peer] = call;
+						sharesIn[call.peer] = video;
 						call.answer();
 					}
 
 					call.on('close', () => {
 						video.remove()
+						if (streamType == "video") {
+							if (peers[call.peer]) {
+								peers[call.peer].close()
+								peers[call.peer] = null;
+							}
+						} else {
+							if (shareCallsIn[call.peer]) {
+								shareCallsIn[call.peer].close()
+								shareCallsIn[call.peer] = null;
+							}
+						}
+
+
 						console.log('close from other end')
 					})
 
 					call.on('stream', userVideoStream => {
 						// add incoming stream to grid
 						addStream(video, userVideoStream);
-						console.log('peer joined');
+						console.log('peer joined ' + call.peer + ' ' + call.metadata.type);
 					})
 				
 				})
@@ -94,15 +129,42 @@
 				
 				// on disconnect, close call
 				socket.on('user-disconnected', userId => {
-					if (peers[userId]) peers[userId].close()
-					if (sharecalls[userId]) sharecalls[userId].close()
+					console.log("can");
+					console.log(peers[userId])
+					if (peers[userId]) {
+						peers[userId].close()
+						peers[userId] = null;
+					}
+					if (shareCallsOut[userId]) {
+						shareCallsOut[userId].close()
+						shareCallsOut[userId] = nul;
+					}
+
+					if (shareCallsIn[userId]) {
+						shareCallsIn[userId].close()
+						shareCallsIn[userId] = null;
+					}
+
 					if (videos[userId]) {
 						videos[userId].remove();
 						videos[userId] = null;
 					}
-					if (shares[userId]) {
-						shares[userId].remove();
-						shares[userId] = null;
+					if (sharesIn[userId]) {
+						sharesIn[userId].remove();
+						sharesIn[userId] = null;
+					}
+
+				})
+
+				socket.on('user-ended-screenshare', userId => {
+					if (sharesIn[userId]) {
+						sharesIn[userId].remove();
+						sharesIn[userId] = null;
+					}
+
+					if (shareCallsIn[userId]) {
+						shareCallsIn[userId].close()
+						shareCallsIn[userId] = null;
 					}
 				})
 			})
@@ -110,76 +172,93 @@
 			// screenshare
 			function screenShare () {
 				// get user screen
+				
+				if (screenshare) {
+					screenshare = false;
+					screenshareTracks[0].stop();
+					screenshareTracks[0].dispatchEvent(new Event("ended"));
+					screenshareTracks[1].stop();
+				} else {
 
-				navigator.mediaDevices.getDisplayMedia({
-					video: true,
-					audio: true
-				}).then(stream => {
-					console.log('testing')
-					socket.emit('request-participants', roomName);
-					// get user screen, add to grid
-					const video = document.createElement("video");
-					video.muted = true;
-					addStream(video, stream);
+					navigator.mediaDevices.getDisplayMedia({
+						video: true,
+						audio: true
+					}).then(stream => {
+						screenshare = true;
+						screenshareTracks = [stream.getVideoTracks()[0], stream.getAudioTracks()[0]];
+						console.log('testing')
+						socket.emit('request-participants', roomName);
+							// get user screen, add to grid
+							const video = document.createElement("video");
+							video.muted = true;
+							myShare = video;
+							addStream(video, stream);
+							
+							socket.on('user-connected', userId => {
+								if (screenshare) {
+									console.log(userId + " peer share");
+									newUserScreenshare(userId, stream);
+								}
 
-					// // call
-					// peer.on('call', call => {
-					// 	// answer
-					// 	console.log(call);
-					// 	call.answer(stream)
-					
-					// 	const video = document.createElement("video");
-					// 	let streamType = call.metadata.type;
-					// 	if (streamType == "video") {
-					// 		videos[call.peer] = video;
-					// 	} else {
-					// 		shares[call.peer] = video;
-					// 	}
+							})
 
-					// 	call.on('close', () => {
-					// 		video.remove()
-					// 		console.log('close from other end')
-					// 	})
-					// 	call.on('stream', userVideoStream => {
-					// 		// add incoming stream to grid
-					// 		addStream(video, userVideoStream);
-					// 		console.log('peer joined');
-					// 	})
-					
-					// })
+							socket.on('participants', users => {
+								console.log(users);
+								if (screenshare) {
+									for (var key in users) {
+										let user = users[key];
+										if (user != myId && shareCallsOut[user] == null) {
+											console.log(user);
+											newUserScreenshare(user, stream);
+										}
+									}
+								}
 
-					
-					socket.on('user-connected', userId => {
-						console.log(userId + " peer share");
-						newUserScreenshare(userId, stream);
-					})
+							})
 
-					socket.on('participants', users => {
-						console.log(users);
-						for (var key in users) {
-							let user = users[key];
-							if (user != myId) {
-								console.log(user);
-								newUserScreenshare(user, stream);
-							}
-						}
-					})
-				})
+							stream.getVideoTracks()[0].onended = function () {
+
+								socket.emit('stop-screenshare', roomName, myId);
+								screenshare = false;
+								video.remove();
+
+								for (var key in shareCallsOut) {
+									shareCallsOut[key].close();
+									shareCallsOut[key] = null;
+								}
+							};
+						})
+				}
+
+
 			}
 			
 			document.getElementById('share').onclick = screenShare;
 
 
 			// add stream to video grid
-			function addStream (video, stream) {
-				document.getElementById('share').onclick = screenShare;
+			function addStream (video, stream, mine=false) {
+
 				video.srcObject = stream
 				video.classList.add("flex");
 				video.classList.add("border-2");
 				video.classList.add("w-full");
-				video.classList.add("h-full");
+				video.classList.add("mx-auto");
+
+				if (mine) {
+					video.classList.add("-scale-x-100");
+				}
+
+
 				video.onclick = function() {
+
 					superVid.srcObject = stream;
+	
+					if (mine) {
+						superVid.classList.add("-scale-x-100");
+					} else {
+						superVid.classList.remove("-scale-x-100");
+					}
 				}
 				video.addEventListener('loadedmetadata', () => {
 					video.play()
@@ -191,6 +270,9 @@
 				let options = {metadata: {"type": "video"}};
 				const call = peer.call(userId, stream, options);
 				const video = document.createElement("video");
+
+				peers[userId] = call;
+				videos[userId] = video;
 
 				call.on('stream', userStream => {
 					addStream(video, userStream);
@@ -204,29 +286,23 @@
 				})
 				
 				// set call
-				peers[userId] = call;
-				videos[userId] = video;
+
 			}
 
 			function newUserScreenshare (userId, stream) {
 				let options = {metadata: {"type": "screenshare"}};
 				const call = peer.call(userId, stream, options);
-				const video = document.createElement("video");
 
-				call.on('stream', userStream => {
-					addStream(video, userStream);
-				})
 				
 				// remove video on close
 				call.on('close', () => {
-					video.remove()
-					shares[userId] = null;
+					shareCallsOut[userId].close();
+					shareCallsOut[userId] = null;
 					console.log('share close from your end')
 				})
 				
 				// set call
-				sharecalls[userId] = call;
-				shares[userId] = video;
+				shareCallsOut[userId] = call;
 			}
 
 		})
@@ -240,32 +316,36 @@
 	<meta name="description" content="Svelte demo app" />
 </svelte:head>
 
-<main class="bg-gray-900 w-full min-h-screen">
-	<h1 class="p-5 text-white">Welcome to Room {roomName}!</h1>
-  
-	<div class="p-10 flex flex-col items-center">
-		<div>
-			<button id="camToggle" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Toggle Camera</button>
-			<button id="toggleMute" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">{muted ? "Unmute" : "Mute"}</button>
-			<button id="share" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Screenshare</button>
+<main class="flex bg-gray-900 w-full h-screen align-items justify-center">
+
+	
+	<div class="flex-auto max-h-screen w-3/4 pb-5">
+		<div class="grid grid-cols-5 h-2/3">
+			<div class = "flex flex-col">
+				<div id="video-grid" class="basis-0 grow overflow-y-auto">
+					<!-- Your content goes here -->
+				</div>
+			</div>
+			
+			<div id="super-grid" class="col-span-4 h-full">
+				<video autoplay id="super-video" class=" w-full h-full"></video>
+			</div>
 		</div>
 
-		<div id="video-grid" class="grid grid-cols-4">
 
-		</div>
 
-		<div id="super-grid" class="h-full w-full">
-			<video autoplay id="super-video" class="border-2 w-full h-full flex"></video>
-		</div>
+		<div class="flipVid"></div>
 
+	</div>
+
+	<div class="flex flex-box fixed bottom-0 w-full bg-blue-950 py-5 align-items justify-center">
+		
+		<button id="camToggle" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Toggle Camera</button>
+		<button id="toggleMute" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">{muted ? "Unmute" : "Mute"}</button>
+		<button id="share" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">{screenshare ? "Stop Screenshare" : "Screenshare"}</button>
 
 	</div>
 
 
 </main>
 
-<style>
-
-
-  
-  </style>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,7 +3,11 @@ const config = {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 
 	theme: {
-		extend: {}
+		extend: {
+			scale: {
+				'-100': '-1',
+			}
+		}
 	},
 
 	plugins: []
