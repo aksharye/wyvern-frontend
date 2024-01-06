1db91e624de7b8731d18eeabe165762286c2e3f8
aksharye <aksharye@gmail.com>
2024-01-06 13:56:32 -0500

    change peerjs location

--- src/routes/[slug]/+page.svelte
+++ src/routes/[slug]/+page.svelte
@@ -56,7 +56,10 @@
 
 		
 		import("peerjs").then(({ default: Peer }) => {
-			const peer = new Peer({host:'https://abalone-sticky-plywood.glitch.me', path='/'});
+			const peer = new Peer({host:'https://abalone-sticky-plywood.glitch.me',
+								secure:true,
+								port:3000,
+								path: '/peerjs'});
 
 			peer.on('open', id => {
 				// emit join room upon open
